25284fcf4344f632313cf21001962181050267b0
simplextech <40868177+simplextech@users.noreply.github.com>
2019-11-15 13:29:52 -0500

    Update SonosController.py

--- SonosController.py
+++ SonosController.py
@@ -196,35 +196,27 @@ class Controller(polyinterface.Controller):
         if self.household is not None:
             for key in self.household:
                 household = self.household[key]
-                # sonos_groups = None
 
                 try:
                     sonos_groups = SonosControl.get_groups(self.sonos, household)
-                except KeyError as ex:
-                    print('shortPoll Sonos Groups Error: ' + ex)
-                    time.sleep(1)
-                    sonos_groups = SonosControl.get_groups(self.sonos, household)
+                    if sonos_groups is not None:
+                        for group in sonos_groups:
+                            group_id = group['id']
+                            coordinator_id = group['coordinatorId']
+                            group_address = 'g' + coordinator_id.split('_')[1][0:-5].lower()
+                            playback_state = group['playbackState']
 
-                # sonos_groups = SonosControl.get_groups(self.sonos, household)
-                if sonos_groups is not None:
-                    for group in sonos_groups:
-                        group_id = group['id']
-                        coordinator_id = group['coordinatorId']
-                        group_address = 'g' + coordinator_id.split('_')[1][0:-5].lower()
-                        playback_state = group['playbackState']
-
-                        if playback_state == 'PLAYBACK_STATE_PLAYING':
-                            playbackstate = 1
-                        elif playback_state == 'PLAYBACK_STATE_TRANSITIONING':
-                            playbackstate = 2
-                        elif playback_state == 'PLAYBACK_STATE_PAUSED':
-                            playbackstate = 3
-                        elif playback_state == 'PLAYBACK_STATE_IDLE':
-                            playbackstate = 4
-                        else:
-                            playbackstate = 0
-                        self.nodes[group_address].setDriver('ST', playbackstate)
-
+                            if playback_state == 'PLAYBACK_STATE_PLAYING':
+                                playbackstate = 1
+                            elif playback_state == 'PLAYBACK_STATE_TRANSITIONING':
+                                playbackstate = 2
+                            elif playback_state == 'PLAYBACK_STATE_PAUSED':
+                                playbackstate = 3
+                            elif playback_state == 'PLAYBACK_STATE_IDLE':
+                                playbackstate = 4
+                            else:
+                                playbackstate = 0
+                            self.nodes[group_address].setDriver('ST', playbackstate)
                         try:
                             group_volume = SonosControl.get_group_volume(self.sonos, household, group_id)
                             # List 0=volume, 1=muted, 2=fixed(true/false)
@@ -236,25 +228,25 @@ class Controller(polyinterface.Controller):
                         except:  # Catch All
                             e = sys.exc_info()[0]
                             print("shortPoll Group Volume Error: " + e)
+                except KeyError as ex:
+                    print('shortPoll Sonos Groups Error: ' + ex)
+                    time.sleep(1)
 
                 try:
                     sonos_players = SonosControl.get_players(self.sonos, household)
+                    if sonos_players is not None:
+                        for player in sonos_players:
+                            player_id = player['id']
+                            player_address = 'p' + player_id.split('_')[1][0:-5].lower()
+                            player_volume = SonosControl.get_player_volume(self.sonos, player_id)
+                            # List 0=volume, 1=muted, 2=fixed(true/false)
+                            self.nodes[player_address].setDriver('SVOL', player_volume[0])
+                            if player_volume[1] == 'true':
+                                self.nodes[player_address].setDriver('GV0', 1)
+                            else:
+                                self.nodes[player_address].setDriver('GV0', 0)
                 except KeyError as ex:
                     print("shortPoll Get Players: " + ex)
-                    time.sleep(1)
-                    sonos_players = SonosControl.get_players(self.sonos, household)
-
-                if sonos_players is not None:
-                    for player in sonos_players:
-                        player_id = player['id']
-                        player_address = 'p' + player_id.split('_')[1][0:-5].lower()
-                        player_volume = SonosControl.get_player_volume(self.sonos, player_id)
-                        # List 0=volume, 1=muted, 2=fixed(true/false)
-                        self.nodes[player_address].setDriver('SVOL', player_volume[0])
-                        if player_volume[1] == 'true':
-                            self.nodes[player_address].setDriver('GV0', 1)
-                        else:
-                            self.nodes[player_address].setDriver('GV0', 0)
 
     def longPoll(self):
         self.refresh_token()
